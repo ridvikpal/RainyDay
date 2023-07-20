48e0818b5abb033732e94bd9064649ed48058141
ridvikpal <pal.ridvik@gmail.com>
2023-07-20 13:59:33 -0400

    Added java-swing as dependency because for some reason that was needed by intellij to compile.

--- src/main/java/com/rainyday/rainyday/HelloApplication.java
+++ src/main/java/com/rainyday/rainyday/HelloApplication.java
@@ -7,16 +7,25 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+// In JavaFX the main class must extend the Application interface
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // load the application setup via the fxml file
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        // set the window (stage) title
         stage.setTitle("Hello!");
+
+        // set the window contents (scene)1
         stage.setScene(scene);
+
+        // show the actual window
         stage.show();
     }
 
+    // launch the program
     public static void main(String[] args) {
         launch();
     }
