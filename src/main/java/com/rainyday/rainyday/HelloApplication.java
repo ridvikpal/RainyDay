9940c7b5e7a2eb8cdb197f3544103c000ddbe410
Ridvik Pal <pal.ridvik@gmail.com>
2023-07-21 12:34:38 -0400

    Added basic ui for fxml file.

--- src/main/java/com/rainyday/rainyday/HelloApplication.java
+++ src/main/java/com/rainyday/rainyday/HelloApplication.java
@@ -9,16 +9,19 @@ import java.io.IOException;
 
 // In JavaFX the main class must extend the Application interface
 public class HelloApplication extends Application {
+    // the start method initializes the program
     @Override
     public void start(Stage stage) throws IOException {
         // load the application setup via the fxml file
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        // create a scene (window content)
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
         // set the window (stage) title
-        stage.setTitle("Hello!");
+        stage.setTitle("RainyDay");
 
-        // set the window contents (scene)1
+        // set the scene (contents) for the stage (window)
         stage.setScene(scene);
 
         // show the actual window
@@ -27,6 +30,6 @@ public class HelloApplication extends Application {
 
     // launch the program
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
