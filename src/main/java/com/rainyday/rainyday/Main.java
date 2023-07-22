62cdaa69050419f528ec0458496fbfc75430608a
Ridvik Pal <pal.ridvik@gmail.com>
2023-07-22 11:41:24 -0400

    testing commit for github repo.

--- src/main/java/com/rainyday/rainyday/Main.java
+++ src/main/java/com/rainyday/rainyday/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        FXMLLoader fxmlLoader = new FXMLLoader();
     }
 
     public static void main(String args[]){
