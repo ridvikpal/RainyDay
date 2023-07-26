fdb9a161d594dda780dfb67b77206c69da4667c6
Ridvik Pal <pal.ridvik@gmail.com>
2023-07-25 22:40:59 -0400

    working different themes.

--- src/main/java/com/rainyday/rainyday/Controller.java
+++ src/main/java/com/rainyday/rainyday/Controller.java
@@ -9,7 +9,6 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -18,7 +17,6 @@ import javafx.scene.text.TextAlignment;
 import org.rainyday.Connection;
 import org.rainyday.Weather;
 
-import java.net.URISyntaxException;
 import java.util.Objects;
 
 public class Controller {
@@ -211,13 +209,11 @@ public class Controller {
         // set the correct alert
         setAlert(weather);
 
-        // set the correct color theme and color theme
+        // set the correct color theme based on the condition
         if (weather.getCurrent().getIs_day() == 1){
-//            setDayImage(weather.getCurrent().getCondition().getCode());
-            setLightTheme();
+            setLightTheme(weather.getCurrent().getCondition().getCode());
         }else{
-//            setNightImage(weather.getCurrent().getCondition().getCode());
-            setDarkTheme();
+            setDarkTheme(weather.getCurrent().getCondition().getCode());
         }
 
         // set the charts
@@ -240,7 +236,7 @@ public class Controller {
 
     }
 
-    void setLightTheme(){
+    void setLightTheme(int _weatherCode){
         // set the right text colours
         lastUpdatedTimeText.setFill(Color.BLACK);
         currentTempText.setFill(Color.BLACK);
@@ -266,15 +262,46 @@ public class Controller {
         lastUpdatedTimeText.setFill(Color.BLACK);
 
         try {
-            splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
-                    .getResource("styles/hail_day_theme.css"))
-                    .toURI().toString());
+            switch (_weatherCode){
+                // set sunny day theme
+                case 1000, 1003 -> splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
+                        .getResource("styles/sunny_day_theme.css"))
+                        .toURI().toString());
+                // set cloudy day theme
+                case 1006, 1009 -> splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
+                        .getResource("styles/cloudy_day_theme.css"))
+                        .toURI().toString());
+                // set foggy day theme
+                case 1030, 1135, 1147 -> splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
+                        .getResource("styles/foggy_day_theme.css"))
+                        .toURI().toString());
+                // set rainy day theme
+                case 1063, 1069, 1072, 1150, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1204,
+                        1207, 1240, 1243, 1246, 1249, 1252 -> splitPaneScene.getStylesheets()
+                        .setAll(Objects.requireNonNull(getClass()
+                        .getResource("styles/rain_day_theme.css"))
+                        .toURI().toString());
+                // set snowy day theme
+                case 1066, 1114, 1117, 1210, 1213, 1216, 1219, 1222, 1225, 1255, 1258 -> splitPaneScene.getStylesheets()
+                        .setAll(Objects.requireNonNull(getClass()
+                        .getResource("styles/snowy_day_theme.css"))
+                        .toURI().toString());
+                // set thunderstorm theme
+                case 1087, 1273, 1276, 1279, 1282 -> splitPaneScene.getStylesheets().setAll(Objects
+                        .requireNonNull(getClass()
+                        .getResource("styles/thunderstorm_day_theme.css"))
+                        .toURI().toString());
+                // set hail theme
+                case 1237, 1261, 1264 -> splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
+                        .getResource("styles/hail_day_theme.css"))
+                        .toURI().toString());
+            }
         } catch (Exception e) {
             System.out.println("Error setting light theme");
         }
     }
 
-    void setDarkTheme(){
+    void setDarkTheme(int _weatherCode){
         // set the right text colours
         lastUpdatedTimeText.setFill(Color.WHITE);
         currentTempText.setFill(Color.WHITE);
@@ -301,9 +328,40 @@ public class Controller {
 
 
         try {
-            splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
-                     .getResource("styles/hail_night_theme.css"))
-                    .toURI().toString());
+            switch (_weatherCode){
+                // set clear night theme
+                case 1000, 1003 -> splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
+                                .getResource("styles/clear_night_theme.css"))
+                        .toURI().toString());
+                // set cloudy night theme
+                case 1006, 1009 -> splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
+                                .getResource("styles/cloudy_night_theme.css"))
+                        .toURI().toString());
+                // set foggy night theme
+                case 1030, 1135, 1147 -> splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
+                                .getResource("styles/foggy_night_theme.css"))
+                        .toURI().toString());
+                // set rainy night theme
+                case 1063, 1069, 1072, 1150, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1204,
+                        1207, 1240, 1243, 1246, 1249, 1252 -> splitPaneScene.getStylesheets()
+                        .setAll(Objects.requireNonNull(getClass()
+                                        .getResource("styles/rain_night_theme.css"))
+                                .toURI().toString());
+                // set snowy night theme
+                case 1066, 1114, 1117, 1210, 1213, 1216, 1219, 1222, 1225, 1255, 1258 -> splitPaneScene.getStylesheets()
+                        .setAll(Objects.requireNonNull(getClass()
+                                        .getResource("styles/snowy_night_theme.css"))
+                                .toURI().toString());
+                // set thunderstorm theme
+                case 1087, 1273, 1276, 1279, 1282 -> splitPaneScene.getStylesheets().setAll(Objects
+                        .requireNonNull(getClass()
+                                .getResource("styles/thunderstorm_night_theme.css"))
+                        .toURI().toString());
+                // set hail theme
+                case 1237, 1261, 1264 -> splitPaneScene.getStylesheets().setAll(Objects.requireNonNull(getClass()
+                                .getResource("styles/hail_night_theme.css"))
+                        .toURI().toString());
+            }
         } catch (Exception e) {
             System.out.println("Error setting dark theme");
         }
